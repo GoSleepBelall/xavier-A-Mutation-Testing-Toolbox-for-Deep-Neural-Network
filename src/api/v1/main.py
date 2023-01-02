c11696555b234722009d2725a6170b1b2a697493
GoSleepBelall <f190305@cfd.nu.edu.pk>
2023-01-03 00:55:17 +0500

    feat: added GET request for trainable_weights

--- src/api/v1/main.py
+++ src/api/v1/main.py
@@ -1,8 +1,34 @@
 from typing import Union
-
 from fastapi import FastAPI
+import jsonify as json
+import tensorflow as tf
+
+from src.main.models_generator import Lenet5Generator
+from src.main.operator_utils import WeightUtils
+from src.main.operator_utils import Model_layers
+
+
 
 app = FastAPI()
+layers = Lenet5Generator()
+weights = WeightUtils()
+
+
+# Read the models globally
+
+lenet5 = tf.keras.models.load_model("../../models/xavier-lenet5.h5")
+alexnet = tf.keras.models.load_model("../../models/xavier-lenet5.h5")       #TODO: this is still reading lenet-5 because i dont have alexnet right now
+
+
+@app.get("/weights/{model}/{layerName}")
+def get_weights(model: str, layerName: str):
+    if model == "1":
+        model = lenet5
+    elif model == "2":
+        model = alexnet
+    trainable_weights = weights.GetWeights(model, layerName)
+    print(trainable_weights)
+    return json(trainable_weights)
 
 
 @app.get("/")
