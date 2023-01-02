bbab65b95119cf6e0ca1960bd88df92a0665a335
GoSleepBelall <f190305@cfd.nu.edu.pk>
2023-01-03 01:15:01 +0500

    feat: Added GET requests for layer names and kernel weights
    Not Tested

--- src/api/v1/main.py
+++ src/api/v1/main.py
@@ -1,6 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
-import jsonify as json
+import json
 import tensorflow as tf
 
 from src.main.models_generator import Lenet5Generator
@@ -10,7 +10,7 @@ from src.main.operator_utils import Model_layers
 
 
 app = FastAPI()
-layers = Lenet5Generator()
+layers = Model_layers()
 weights = WeightUtils()
 
 
@@ -20,6 +20,7 @@ lenet5 = tf.keras.models.load_model("../../models/xavier-lenet5.h5")
 alexnet = tf.keras.models.load_model("../../models/xavier-lenet5.h5")       #TODO: this is still reading lenet-5 because i dont have alexnet right now
 
 
+# GET request to retrieve all the trainable weights of a particular layer in a specific model
 @app.get("/weights/{model}/{layerName}")
 def get_weights(model: str, layerName: str):
     if model == "1":
@@ -28,7 +29,32 @@ def get_weights(model: str, layerName: str):
         model = alexnet
     trainable_weights = weights.GetWeights(model, layerName)
     print(trainable_weights)
-    return json(trainable_weights)
+    return json.dumps(trainable_weights)
+
+
+# GET request to retrieve all the layers in a specific model
+@app.get("/layers/{model}")
+def get_layers(model: str):
+    if model == "1":
+        model = lenet5
+    elif model == "2":
+        model = alexnet
+    layer_names = layers.getLayerNames(model)
+    return json.dumps(layer_names)
+
+
+# GET request to retrieve all the weights of a specific kernel in a specific layer of a specific model
+@app.get("/weights/{model}/{layerName}/{kernel}")
+def getKernelWeights(model: str, layerName: str, kernel: int):
+    if model == "1":
+        model = lenet5
+    elif model == "2":
+        model = alexnet
+    trainable_weights = weights.GetWeights(model, layerName)
+    kernel_weights = weights.getKernelWeights(trainable_weights, kernel)
+    return json.dumps(kernel_weights)
+
+
 
 
 @app.get("/")
