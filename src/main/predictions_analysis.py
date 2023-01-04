8a5ad9e4828d2a9de32a1082f969c797f2b3aa72
GoSleepBelall <f190305@cfd.nu.edu.pk>
2023-01-04 06:41:28 +0500

    feat: Calculated Confusion Matrix from Predictions and Labels

--- src/main/predictions_analysis.py
+++ src/main/predictions_analysis.py
@@ -5,6 +5,9 @@ from tensorflow import keras
 import numpy as np
 from keras.utils import np_utils
 from sklearn.metrics import classification_report
+from prettytable import PrettyTable
+
+
 
 def float_formatter(num):
     return "{:.1f}".format(num)
@@ -35,16 +38,59 @@ def map_manual_predictions(predictions,test_data, classes, count):
     print("labels")
     matrix_mapper(classified_test_data,count)
 
+def get_confusion_matrix(predictions, labels):
+    # Reshaping Predictions (Merging data)
+    predictions = np.argmax(predictions, axis=1)
+
+    # Initialize counters for each class
+    counters = {
+        0: {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0},
+        1: {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0},
+        2: {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0},
+        3: {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0},
+        4: {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0},
+        5: {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0},
+        6: {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0},
+        7: {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0},
+        8: {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0},
+        9: {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0},
+    }
+    # Loop over the elements in the predictions and labels arrays
+    for i in range(len(predictions)):
+        # Get the prediction and label for the current element
+        prediction = predictions[i]
+        label = labels[i]
+
+        # Increment the appropriate counter based on the prediction and label
+        if prediction == label:
+            # TRUE-POSITIVE case: Model accurately predicted the label
+            counters[prediction]['tp'] += 1
+            # TRUE-NEGATIVE case: Model accurately predicted that label does not exist
+            for class_label in range(10):
+                if class_label != prediction:
+                    counters[class_label]['tn'] += 1
+        else:
+            # Model predicted a wrong label
+            counters[prediction]['fp'] += 1
+            # Model did not predict the label when it existed
+            counters[label]['fn'] += 1
+    return counters
+
+
+def printConfusionMatrix(predictions, labels):
+    counters = get_confusion_matrix(predictions, labels)
+    table = PrettyTable()
+    table.field_names = ['Class', 'True-Positive', 'True-Negative', 'False-Positive', 'False-Negative']
+
+    # Add a row for each class to the table
+    for class_label, class_counters in counters.items():
+        table.add_row([class_label, class_counters['tp'], class_counters['tn'], class_counters['fp'], class_counters['fn']])
+    # Print the table
+    print(table)
 
 
 def generate_classification_report(predictions, labels):
     # Reshaping Predictions (Merging data)
     predictions = np.argmax(predictions, axis=1)
-    # Generate Classification Report
+    # Generate Automatic Classification Report
     print(classification_report(labels, predictions))
-
-
-
-
-
-
